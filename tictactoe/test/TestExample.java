5729dea7dc40fb30594313d735ffdf8d6ea075bf
Omar Sanchez <osanchez@umass.edu>
2018-10-14 23:23:49 -0400

    created model, view, controller

--- tictactoe/test/TestExample.java
+++ tictactoe/test/TestExample.java
@@ -8,12 +8,6 @@ import static org.junit.Assert.*;
  * An example test class, which merely shows how to write JUnit tests.
  */
 public class TestExample {
-    @Test
-    public void testNewGame() {
-        View game = new View();
-        assertEquals (1, game.player);
-        assertEquals (9, game.movesLeft);
-    }
 
     @Test
     public void testModel() {
@@ -21,12 +15,13 @@ public class TestExample {
     }
 
     @Test
+    public void testView() {
+        View view = new View();
+    }
+
+    @Test
     public void testController() {
         Controller controller = new Controller();
     }
 
-    @Test
-    public void testView() {
-        View view = new View();
-    }
 }
